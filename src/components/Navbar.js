dff76ac2af7f5b9e8890385adde78b29ea62dae7
fenice <danielhan@Daniels-MacBook-Air.local>
2019-03-17 04:44:47 -0400

    fooh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = props => (
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+    <nav className="navbar navbar-expand-lg fixed-top">
         <a className="navbar-brand" href="/" id="logo"><h3>Clicky Game</h3></a>
         <h5 className="ml-auto mt-2" id="counters">Score: {props.score} | Highest score: {props.top} </h5>
     </nav>
